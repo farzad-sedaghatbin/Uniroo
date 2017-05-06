275bffe5c80e9310659c930ea3b402d26b795924
omid.javaheri66@gmail.com <omid.javaheri66@gmail.com>
2017-05-06 17:26:40 +0430

    login & signup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,129 @@
 angular.module('starter.controllers', [])
 
+.controller('loginCtrl', function ($scope, $rootScope, $ionicModal, $timeout, $state, $ionicLoading, $ionicPopup, $http, $ionicHistory, WebService) {
+  $scope.closeLogin = function () {
+    $scope.mainModal.hide();
+  };
+
+  $scope.closeSignUp = function () {
+    $scope.mainModal.hide();
+  };
+
+
+  // Open the login modal
+  $scope.show_login1 = function () {
+    $ionicModal.fromTemplateUrl('templates/login-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $rootScope.mainModal = modal;
+      $rootScope.mainModal.show();
+    });
+  };
+
+  $scope.sign_up = function () {
+    $ionicModal.fromTemplateUrl('templates/sign-up-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $rootScope.mainModal = modal;
+      $rootScope.mainModal.show();
+    });
+  };
+
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function (form) {
+    WebService.startLoading();
+    //$state.go('view', {movieid: 1});
+    // $state.go('app.landing');
+    if (form.$valid) {
+      try {
+        delete $http.defaults.headers.common.Authorization;
+      }catch (e){
+      }
+      var url = "http://192.168.161.111:8080/api/1/user_authenticate";
+      var data = {
+        username: $scope.login.mail,
+        password: $scope.login.pwd,
+        rememberMe: false
+      };
+      $http.post(url, data).success(function (data, status, headers, config) {
+        WebService.stopLoading();
+        $rootScope.username = $scope.login.mail;
+        $http.defaults.headers.common.Authorization = "Bearer " + data.token;
+        var db = openDatabase('mydb', '1.0', 'Test DB', 1024 * 1024);
+        db.transaction(function (tx) {
+          tx.executeSql('INSERT INTO ANIJUU (name, log) VALUES (?, ?)', ["username", $scope.login.mail]);
+          tx.executeSql('INSERT INTO ANIJUU (name, log) VALUES (?, ?)', ["myToken", "Bearer " + data.token]);
+        });
+        $scope.modal.sign_in.hide();
+        $state.go('app.landing', {}, {reload: true});
+      }).catch(function (err) {
+        WebService.stopLoading();
+        WebService.myErrorHandler(err,true);
+      });
+    } else {
+      form.mail.$setDirty();
+      form.pwd.$setDirty();
+    }
+
+  };
+
+  $scope.signUp = {};
+  $scope.do_signUp = function (form) {
+    WebService.startLoading();
+    //$state.go('view', {movieid: 1});
+    if (
+      form.$valid
+      && $scope.signUp.pwd == $scope.signUp.c_pwd
+    //true
+    ) {
+      var post_data = {
+        'secret_key': secret_key,
+        'Email': $scope.signUp.mail,
+        'Password': $scope.signUp.pwd,
+        'Mobile': $scope.signUp.mobile,
+        'User_name': $scope.signUp.user_name,
+        'Name': $scope.signUp.name,
+      }
+
+      var url = "http://127.0.0.1:8080/api/1/signup";
+      var data = {
+        firstName: $scope.signUp.name,
+        lastName: $scope.signUp.name,
+        username: $scope.signUp.user_name,
+        mobile: $scope.signUp.mobile,
+        password: $scope.signUp.pwd
+      };
+      $http.post(url, data)
+        .success(function (suc) {
+          WebService.stopLoading();
+          $state.go("app.landing");
+        }).error(function (err) {
+        WebService.stopLoading();
+        WebService.myErrorHandler(err,false);
+      });
+    } else {
+      form.pwd.$setDirty();
+      form.number.$setDirty();
+      form.mail.$setDirty();
+      form.name.$setDirty();
+      form.user_name.$setDirty();
+
+    }
+
+  };
+  $scope.logout = function () {
+      $ionicHistory.nextViewOptions({
+        disableAnimate: true,
+        disableBack: true
+      });
+      $state.go('login', {}, {reload: true});
+  };
+})
+
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
   // Form data for the login modal
   $scope.loginData = {};
 
