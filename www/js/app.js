a047e8a277b2f8c076c1d146df95f6038371fd33
Omid <omiddagala@yahoo.com>
2017-05-11 09:55:19 +0430

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -4,222 +4,313 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
 /* //if use wakanda platform
-    angular.module('starter', ['ionic', 'starter.controllers','wakanda'])
-*/
-.run(function($ionicPlatform,$rootScope,$location,$ionicScrollDelegate,$ionicPopup) {
+ angular.module('starter', ['ionic', 'starter.controllers','wakanda'])
+ */
+  .run(function ($ionicPlatform, $rootScope, $location, $ionicScrollDelegate, $ionicPopup, $http) {
 
-/*************** forget password ****************/
+    /*************** forget password ****************/
 
-     $rootScope.forget_password=function (){
-        $ionicPopup.show({
+    $rootScope.forget_password = function () {
+      $ionicPopup.show({
         template: 'Enter your email address below.<label class="item item-input" style="  height: 34px; margin-top: 10px;"><input  type="email"  /></label>',
         title: 'Forget Password',
         subTitle: ' ',
         scope: $rootScope,
         buttons: [
-        {text: 'Send',
-        type: 'button-clear dark-blue'},
-        { text: 'Cancel' ,
-        type: 'button-clear main-bg-color'},]
-        });
+          {
+            text: 'Send',
+            type: 'button-clear dark-blue'
+          },
+          {
+            text: 'Cancel',
+            type: 'button-clear main-bg-color'
+          },]
+      });
     };
 
-/*************** increment-decrement function ****************/
-$rootScope.valueKids=1;
- $rootScope.valueAdults=1;
- $rootScope.valueBabies=1;
-  $rootScope.increment_val= function(type) {
-    if (type=='Kids'&&$rootScope.valueKids >= 0) $rootScope.valueKids++;
-    if (type=='Adults'&&$rootScope.valueAdults >= 0) $rootScope.valueAdults++;
-    if (type=='Babies'&&$rootScope.valueBabies >= 0) $rootScope.valueBabies++;
-  };
-  $rootScope.decrement_val = function(type) {
-    //if ($rootScope.value > 0)  $rootScope.value--;
-    if (type=='Kids'&&$rootScope.valueKids > 0) $rootScope.valueKids--;
-    if (type=='Adults'&&$rootScope.valueAdults > 0) $rootScope.valueAdults--;
-    if (type=='Babies'&&$rootScope.valueBabies > 0) $rootScope.valueBabies--;
+    /*************** increment-decrement function ****************/
+    $rootScope.valueKids = 1;
+    $rootScope.valueAdults = 1;
+    $rootScope.valueBabies = 1;
+    $rootScope.increment_val = function (type) {
+      if (type == 'Kids' && $rootScope.valueKids >= 0) $rootScope.valueKids++;
+      if (type == 'Adults' && $rootScope.valueAdults >= 0) $rootScope.valueAdults++;
+      if (type == 'Babies' && $rootScope.valueBabies >= 0) $rootScope.valueBabies++;
+    };
+    $rootScope.decrement_val = function (type) {
+      //if ($rootScope.value > 0)  $rootScope.value--;
+      if (type == 'Kids' && $rootScope.valueKids > 0) $rootScope.valueKids--;
+      if (type == 'Adults' && $rootScope.valueAdults > 0) $rootScope.valueAdults--;
+      if (type == 'Babies' && $rootScope.valueBabies > 0) $rootScope.valueBabies--;
 
-  };
+    };
 
-$rootScope.confirmMsg=function(index){
-    $rootScope.show_msg=index
-}
+    $rootScope.confirmMsg = function (index) {
+      $rootScope.show_msg = index
+    }
 
- $rootScope.scrollTop = function() {
-    $ionicScrollDelegate.scrollTop();
-  };
- /*************** group function ****************/
- $rootScope.groups = [
-    {id: 1, items: [{ subName: 'SubBubbles1'}]},
+    $rootScope.scrollTop = function () {
+      $ionicScrollDelegate.scrollTop();
+    };
+    /*************** group function ****************/
+    $rootScope.groups = [
+      {id: 1, items: [{subName: 'SubBubbles1'}]},
 
-    {id: 2, items: [{ subName: 'SubBubbles1'}]},
+      {id: 2, items: [{subName: 'SubBubbles1'}]},
 
-    {id: 3, items: [{ subName: 'SubBubbles1'}]},
+      {id: 3, items: [{subName: 'SubBubbles1'}]},
 
-    {id: 4, items: [{ subName: 'SubBubbles1'}]},
+      {id: 4, items: [{subName: 'SubBubbles1'}]},
 
-    {id: 5, items: [{ subName: 'SubBubbles1'}]},
+      {id: 5, items: [{subName: 'SubBubbles1'}]},
 
-    {id: 6, items: [{ subName: 'SubBubbles1'}]},
+      {id: 6, items: [{subName: 'SubBubbles1'}]},
 
-    {id: 7, items: [{ subName: 'SubBubbles1'}]}
-  ];
+      {id: 7, items: [{subName: 'SubBubbles1'}]}
+    ];
 
 
     /*
-   * if given group is the selected group, deselect it
-   * else, select the given group
-   */
-  $rootScope.toggleGroup = function(group) {
-    if ($rootScope.isGroupShown(group)) {
-      $rootScope.shownGroup = null;
-    } else {
-      $rootScope.shownGroup = group;
-    }
-  };
-  $rootScope.isGroupShown = function(group) {
-    return $rootScope.shownGroup === group;
-  };
+     * if given group is the selected group, deselect it
+     * else, select the given group
+     */
+    $rootScope.toggleGroup = function (group) {
+      if ($rootScope.isGroupShown(group)) {
+        $rootScope.shownGroup = null;
+      } else {
+        $rootScope.shownGroup = group;
+      }
+    };
+    $rootScope.isGroupShown = function (group) {
+      return $rootScope.shownGroup === group;
+    };
 
-/*************** location function ****************/
-  $rootScope.goto=function(url){
+    /*************** location function ****************/
+    $rootScope.goto = function (url) {
       $location.path(url)
-  }
-
-/*************** active function ****************/
-$rootScope.activeIcon=1
- $rootScope.activeTab=function(index){
-      $rootScope.activeIcon=index
-  }
-/*************** repeat array ****************/
-        $rootScope.menu =[{id:"1",img:"img/1.png",title:"جست و جو",link:"#/app/search"},
-                        {id:"2",img:"img/2.png",title:"سفرهای من",link:"#/app/reservations"},
-                        {id:"3",img:"img/3.png",title:"پیشنهاد ها",link:"#/app/offer"},
-                        {id:"4",img:"img/4.png",title:"ثبت نام",link:"#/app/register"},
-                        {id:"5",img:"img/5.png",title:"تماس با ما",link:"#/app/contact"},
-                        {id:"6",img:"img/6.png",title:"درباره ما",link:"#/app/about"}]
-
-        $rootScope.det =[{id:"1"},
-                        {id:"2"}]
-
-    $rootScope.data =[{id:"1"},
-                        {id:"2"},{id:"3"},{id:"4"},{id:"5"}]
-
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+
+    /*************** active function ****************/
+    $rootScope.activeIcon = 1
+    $rootScope.activeTab = function (index) {
+      $rootScope.activeIcon = index
     }
-  });
-})
+    /*************** repeat array ****************/
+    $rootScope.menu = [{id: "1", img: "img/1.png", title: "جست و جو", link: "#/app/search"},
+      {id: "2", img: "img/2.png", title: "سفرهای من", link: "#/app/reservations"},
+      {id: "3", img: "img/3.png", title: "پیشنهاد ها", link: "#/app/offer"},
+      {id: "4", img: "img/4.png", title: "ثبت نام", link: "#/app/register"},
+      {id: "5", img: "img/5.png", title: "تماس با ما", link: "#/app/contact"},
+      {id: "6", img: "img/6.png", title: "درباره ما", link: "#/login"}]
 
-.config(function($stateProvider, $urlRouterProvider,$ionicConfigProvider) {
+    $rootScope.det = [{id: "1"},
+      {id: "2"}]
 
-  $ionicConfigProvider.navBar.alignTitle('center');
-  $ionicConfigProvider.backButton.text('').previousTitleText('');
+    $rootScope.data = [{id: "1"},
+      {id: "2"}, {id: "3"}, {id: "4"}, {id: "5"}]
+
+    $ionicPlatform.ready(function () {
+      var client = new WebSocket("ws://192.168.161.111:8080/myHandler");
+      client.onopen = function () {
+        client.send("join,1");
+      };
+      client.onmessage = function (msg) {
+        var data = JSON.parse(msg.data);
+        switch (data.command) {
+          case "aroundme":
+
+            break;
+          case "driverinfo":
+
+            break;
+          case "delivery":
+
+            break;
+        }
+      };
+      $rootScope.$on('$cordovaNetwork:online', function (event, networkState) {
+        set_net('online');
+      });
+      $rootScope.$on('$cordovaNetwork:offline', function (event, networkState) {
+        set_net('offline');
+      });
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+    var db = openDatabase('mydb', '1.0', 'Test DB', 1024 * 1024);
+    db.transaction(function (tx) {
+      tx.executeSql('SELECT d.log FROM ANIJUU d WHERE d.name="username"', [], function (tx, results) {
+        var len = results.rows.length, i, result = '';
+        if (!results.rows || results.rows.length == 0) {
+          result = null;
+        } else {
+          result = results.rows.item(0).log;
+        }
+        setUsername(result)
+      }, null);
+    });
+    var setUsername = function (result) {
+      $rootScope.username = result;
+    };
+    db.transaction(function (tx) {
+      tx.executeSql('SELECT d.log FROM ANIJUU d WHERE d.name="myToken"', [], function (tx, results) {
+        var len = results.rows.length, i, result = '';
+        if (!results.rows || results.rows.length == 0) {
+          result = null;
+        } else {
+          result = results.rows.item(0).log;
+        }
+        setToken(result)
+      }, null);
+    });
+    var setToken = function (result) {
+      if (result) {
+        $http.defaults.headers.common.Authorization = result;
+      } else {
+        try {
+          delete $http.defaults.headers.common.Authorization;
+        } catch (e) {
+        }
+      }
+    }
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    $ionicConfigProvider.navBar.alignTitle('center');
+    $ionicConfigProvider.backButton.text('').previousTitleText('');
 
 
-  $stateProvider
+    $stateProvider
 
-   .state('home', {
-    url: "/home",
+      .state('home', {
+        url: "/home",
         templateUrl: "templates/home.html"
-  })
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      })
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+      })
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
+      .state('app.search', {
+        url: "/search",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/search.html",
+            controller : "SearchCtrl"
+          }
+        }
+      })
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
+      .state('app.payment', {
+        url: "/payment",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/payment.html"
+          }
+        }
+      })
 
-    .state('app.payment', {
-    url: "/payment",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/payment.html"
-      }
-    }
-  })
+      .state('app.contact', {
+        url: "/contact",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/contact.html"
+          }
+        }
+      })
 
-  .state('app.contact', {
-    url: "/contact",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/contact.html"
-      }
-    }
-  })
+      .state('app.reservations', {
+        url: "/reservations",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/reservations.html"
+          }
+        }
+      })
 
-  .state('app.reservations', {
-    url: "/reservations",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/reservations.html"
-      }
-    }
-  })
+      .state('app.details', {
+        url: "/details",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/details.html"
+          }
+        }
+      })
 
-  .state('app.details', {
-    url: "/details",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/details.html"
-      }
-    }
-  })
+      .state('app.data', {
+        url: "/data",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/data.html",
+            controller : "DataCtrl"
+          }
+        }
+      })
 
-  .state('app.data', {
-    url: "/data",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/data.html"
-      }
-    }
-  })
+      .state('app.offer', {
+        url: "/offer",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/offer.html"
+          }
+        }
+      })
 
-  .state('app.offer', {
-    url: "/offer",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/offer.html"
-      }
-    }
-  })
+      .state('app.register', {
+        url: "/register",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/register.html",
+            controller:"SignupCtrl"
+          }
+        }
+      })
 
-  .state('app.register', {
-    url: "/register",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/register.html"
-      }
-    }
-  })
-
-  .state('app.about', {
-    url: "/about",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/about.html"
-      }
-    }
+      .state('app.about', {
+        url: "/about",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/about.html"
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise(function ($injector, $location, $http) {
+      var db = openDatabase('mydb', '1.0', 'Test DB', 1024 * 1024);
+      db.transaction(function (tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS ANIJUU (name , log)');
+        tx.executeSql('SELECT d.log FROM ANIJUU d WHERE d.name="username"', [], function (tx, results) {
+          var len = results.rows.length, i, result = '';
+          if (!results.rows || results.rows.length == 0) {
+            $location.path('/login');
+          } else {
+            $location.path('/home');
+          }
+        }, null);
+      });
+    });
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/home');
-});
+function set_net(status) {
+  if (status == 'online') {
+    $('.net-error').hide();
+  } else {
+    $('.net-error').show();
+  }
+}
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,305 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  // Form data for the login modal
-  $scope.loginData = {};
+  .controller('loginCtrl', function ($scope, $rootScope, $ionicModal, $timeout, $state, $ionicLoading, $ionicPopup, $http, $ionicHistory, WebService) {
+    $scope.closeLogin = function () {
+      $scope.mainModal.hide();
+    };
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.closeSignUp = function () {
+      $scope.mainModal.hide();
+    };
+
+
+    // Open the login modal
+    $scope.show_login1 = function () {
+      $ionicModal.fromTemplateUrl('templates/login-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function (modal) {
+        $rootScope.mainModal = modal;
+        $rootScope.mainModal.show();
+      });
+    };
+
+    $scope.sign_up = function () {
+      $state.go("app.register")
+    };
+
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function (form) {
+      WebService.startLoading();
+      //$state.go('view', {movieid: 1});
+      // $state.go('app.landing');
+      if (form.$valid) {
+        try {
+          delete $http.defaults.headers.common.Authorization;
+        } catch (e) {
+        }
+        var url = "http://192.168.161.111:8080/api/1/user_authenticate";
+        var data = {
+          username: $scope.login.mail,
+          password: $scope.login.pwd,
+          rememberMe: false
+        };
+        $http.post(url, data).success(function (data, status, headers, config) {
+          WebService.stopLoading();
+          $rootScope.username = $scope.login.mail;
+          $http.defaults.headers.common.Authorization = "Bearer " + data.token;
+          var db = openDatabase('mydb', '1.0', 'Test DB', 1024 * 1024);
+          db.transaction(function (tx) {
+            tx.executeSql('INSERT INTO ANIJUU (name, log) VALUES (?, ?)', ["username", $scope.login.mail]);
+            tx.executeSql('INSERT INTO ANIJUU (name, log) VALUES (?, ?)', ["myToken", "Bearer " + data.token]);
+          });
+          $scope.modal.sign_in.hide();
+          $state.go('app.landing', {}, {reload: true});
+        }).catch(function (err) {
+          WebService.stopLoading();
+          WebService.myErrorHandler(err, true);
+        });
+      } else {
+        form.mail.$setDirty();
+        form.pwd.$setDirty();
+      }
+
+    };
+
+    $scope.signUp = {};
+    $scope.logout = function () {
+      $ionicHistory.nextViewOptions({
+        disableAnimate: true,
+        disableBack: true
+      });
+      $state.go('login', {}, {reload: true});
+    };
+  })
+
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
+  .controller('SignupCtrl', function ($scope, $ionicModal, $timeout, $ionicPopup, $rootScope, $state) {
+    $timeout(function () {
+      $(".buttons-right").css("display", "none");
+      $(".back-button").css("display", "block");
+    }, 0);
+    $scope.isPassengerParam = true;
+    $scope.isPassenger = function (isPass) {
+      $scope.isPassengerParam = isPass;
+      $scope.driverItems = [];
+    };
+    $scope.chose = function (type) {
+      $scope.type = type;
+      $ionicModal.fromTemplateUrl('templates/select.html', {
+        scope: $scope
+      }).then(function (modal) {
+        $rootScope.mainModal = modal;
+        $rootScope.mainModal.show();
+      });
+    };
+    function setVariable(result) {
+      if ($scope.type == 'STUDENT') {
+        $scope.student = result;
+      } else if ($scope.type == 'DRIVER') {
+        $scope.driver = result;
+      } else if ($scope.type == 'LICENSE') {
+        $scope.license = result;
+      } else if ($scope.type == 'CAR') {
+        $scope.car = result;
+      } else {
+        $scope.insurance = result;
+      }
+    }
+
+    $scope.remove = function (item) {
+      if ($scope.type == 'STUDENT') {
+        $scope.student = null;
+      } else if (item.type == 'DRIVER') {
+        $scope.driver = null;
+      } else if (item.type == 'LICENSE') {
+        $scope.license = null;
+      } else if (item.type == 'CAR') {
+        $scope.car = null;
+      } else {
+        $scope.insurance = null;
+      }
+      $.each($scope.driverItems, function (index, value) {
+        if (value.thumbnail == item.thumbnail) {
+          $scope.driverItems.splice(index, 1);
+        }
+      });
+    };
+    $scope.driverItems = [];
+    $scope.gallery = function () {
+      var options = {sourceType: Camera.PictureSourceType.PHOTOLIBRARY};
+      navigator.camera.getPicture(function cameraSuccess(imageUri) {
+        window.resolveLocalFileSystemURL(imageUri, function (fileEntry) {
+          fileEntry.file(function (file) {
+            var reader = new FileReader();
+            reader.onloadend = function (evt) {
+              //todo: farzad breakpoint
+              setVariable(evt.target.result);
+              $scope.driverItems.push({
+                thumbnail: imageUri,
+                type: $scope.type
+              });
+              $scope.$apply();
+            };
+            reader.readAsDataURL(file);
+          });
+        });
+      }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+      }, options);
+    };
+    $scope.camera = function () {
+      var options = {sourceType: Camera.PictureSourceType.CAMERA};
+      navigator.camera.getPicture(function cameraSuccess(imageUri) {
+        window.resolveLocalFileSystemURL(imageUri, function (fileEntry) {
+          fileEntry.file(function (file) {
+            var reader = new FileReader();
+            reader.onloadend = function (evt) {
+              setVariable(evt.target.result);
+              $scope.driverItems.push({
+                thumbnail: imageUri,
+                type: $scope.type
+              });
+              $scope.$apply();
+            };
+            reader.readAsDataURL(file);
+          });
+        });
+      }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+      }, options);
+    };
+    $scope.do_signUp = function (form) {
+      $state.go("home")
+      // if (!$scope.isPassengerParam) {
+      //   if (!$scope.driver) {
+      //     $ionicPopup.alert({
+      //       title: '<p class="text-center color-yellow">' + "نقص در اطلاعات" + '</p>',
+      //       template: '<p class="text-center color-gery">' + "عکس راننده انتخاب نشده است" + '</p>'
+      //     });
+      //   } else if (!$scope.license) {
+      //     $ionicPopup.alert({
+      //       title: '<p class="text-center color-yellow">' + "نقص در اطلاعات" + '</p>',
+      //       template: '<p class="text-center color-gery">' + "عکس گواهینامه انتخاب نشده است" + '</p>'
+      //     });
+      //   } else if (!$scope.car) {
+      //     $ionicPopup.alert({
+      //       title: '<p class="text-center color-yellow">' + "نقص در اطلاعات" + '</p>',
+      //       template: '<p class="text-center color-gery">' + "عکس کارت ماشین انتخاب نشده است" + '</p>'
+      //     });
+      //   } else if (!$scope.insurance) {
+      //     $ionicPopup.alert({
+      //       title: '<p class="text-center color-yellow">' + "نقص در اطلاعات" + '</p>',
+      //       template: '<p class="text-center color-gery">' + "عکس بیمه نامه انتخاب نشده است" + '</p>'
+      //     });
+      //   }
+      // } else {
+      //   if (!$scope.student) {
+      //     $ionicPopup.alert({
+      //       title: '<p class="text-center color-yellow">' + "نقص در اطلاعات" + '</p>',
+      //       template: '<p class="text-center color-gery">' + "عکس کارت دانشجویی انتخاب نشده است" + '</p>'
+      //     });
+      //   }
+      // }
+      // WebService.startLoading();
+      // //$state.go('view', {movieid: 1});
+      // if (
+      //   form.$valid
+      //   && $scope.signUp.pwd == $scope.signUp.c_pwd
+      // //true
+      // ) {
+      //   var post_data = {
+      //     'secret_key': secret_key,
+      //     'Email': $scope.signUp.mail,
+      //     'Password': $scope.signUp.pwd,
+      //     'Mobile': $scope.signUp.mobile,
+      //     'User_name': $scope.signUp.user_name,
+      //     'Name': $scope.signUp.name,
+      //   }
+      //
+      //   var url = "http://127.0.0.1:8080/api/1/signup";
+      //   var data = {
+      //     firstName: $scope.signUp.name,
+      //     lastName: $scope.signUp.name,
+      //     username: $scope.signUp.user_name,
+      //     mobile: $scope.signUp.mobile,
+      //     password: $scope.signUp.pwd
+      //   };
+      //   $http.post(url, data)
+      //     .success(function (suc) {
+      //       WebService.stopLoading();
+      //       $state.go("app.landing");
+      //     }).error(function (err) {
+      //     WebService.stopLoading();
+      //     WebService.myErrorHandler(err,false);
+      //   });
+      // } else {
+      //   form.pwd.$setDirty();
+      //   form.number.$setDirty();
+      //   form.mail.$setDirty();
+      //   form.name.$setDirty();
+      //   form.user_name.$setDirty();
+      //
+      // }
+
+    };
+
+  })
+  .controller('SearchCtrl', function ($scope, $ionicModal, $timeout, $rootScope, $state) {
+
+  })
+
+  .controller('DataCtrl', function ($scope, $ionicModal, $timeout, $rootScope, $state) {
+    var selectedId;
+    $scope.doSelect = function (item) {
+      if (selectedId) {
+        $('#'+selectedId).prop('checked', false);
+      }
+      selectedId = item.id;
+    }
+  })
+
+  .controller('PlaylistCtrl', function ($scope, $stateParams) {
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+function limitSize(e, id, size) {
+  var keys = [8, 9, 16, 17, 18, 19, 20, 27, 33, 34, 35, 36, 37, 38, 39, 40, 45, 46, 144, 145];
+  if ($.inArray(e.keyCode, keys) == -1) {
+    if ($("#" + id).val().length >= size) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  }
+}
